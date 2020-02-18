de295ce11c078ae7b14435c5127ed0d30da212d1
Jordy Van Kerkvoorde <jordy.vankerkvoorde@student.hogent.be>
2020-02-18 20:38:00 +0100

    Add Testproject + empty test classes

---
+++ ITLab29.Tests/Models/Domain/EventTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ITLab29.Models;
+
+namespace ITLab29.Tests.Models.Domain {
+    class EventTest {
+    }
+}
---
+++ ITLab29.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace ITLab29.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
