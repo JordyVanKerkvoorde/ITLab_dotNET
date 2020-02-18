6e8bb73772c2ec1f0735bac1f800e53111486579
Jan Willem <janwill1996@gmail.com>
2020-02-18 12:11:24 +0100

    Feedback + Location (+enum)

--- ITLab29/Models/Domain/Location.cs
+++ ITLab29/Models/Domain/Location.cs
@@ -7,5 +7,18 @@ namespace ITLab29.Models.Domain
 {
     public class Location
     {
+
+        public string LocationId { get; }
+        public LocationEnum.Campus Campus { get; }
+        public int Capacity { get; set; }
+
+        public Location(string locationId, LocationEnum.Campus campus, int capacity)
+        {
+            LocationId = locationId;
+            Campus = campus;
+            Capacity = capacity;
+        }
+
+
     }
 }
---
+++ ITLab29/Models/Domain/LocationEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab29.Models.Domain
+{
+    public class LocationEnum
+    {
+        public enum Campus
+        {
+            SCHOONMEERSEN,
+            MERCATOR,
+            AALST
+        }
+    }
+}
