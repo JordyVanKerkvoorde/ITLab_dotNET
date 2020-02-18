79fe4916067a16f4024c85ccb7f21687fc62e8b2
Jordy Van Kerkvoorde <jordy.vankerkvoorde@student.hogent.be>
2020-02-18 11:08:48 +0100

    Added Enum Types

---
+++ ITLab29/Models/Domain/MediaEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab29.Models.Domain {
+    public class MediaEnum {
+        public enum MediaType { 
+            IMAGE,
+            VIDEO,
+            FILE
+        }
+    }
+}
---
+++ ITLab29/Models/Domain/UserEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab29.Models.Domain {
+    public class UserEnum {
+        public enum UserStatus { 
+            ACTIVE,
+            BLOCKED,
+            INACTIVE
+        }
+
+        public enum UserType { 
+            ADMIN,
+            RESPONSIBLE,
+            USER
+        }
+    }
+}
