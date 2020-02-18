6bc136eaefcb4df6686d2166ee3196ac06353cc1
Jan Willem <janwill1996@gmail.com>
2020-02-18 11:30:17 +0100

    User class

--- ITLab29/Models/Domain/User.cs
+++ ITLab29/Models/Domain/User.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ITLab29.Models.Domain
 {
@@ -11,6 +8,26 @@ namespace ITLab29.Models.Domain
         public string UserId { get; }
         public string FirstName { get; }
         public string LastName { get; }
-        public int MyProperty { get; set; }
+        public UserEnum.UserType UserType { get; set; }
+        public UserEnum.UserStatus UserStatus { get; set; }
+        public ICollection<Event> Events { get; }
+        public string Email { get; set; }
+
+        public User(string userId, string firstName, string lastName, UserEnum.UserType userType, UserEnum.UserStatus userStatus, string email)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            UserType = userType;
+            UserStatus = userStatus;
+            Email = email;
+        }
+
+        public void AddE(Event e) {
+
+        }
+
+
+
     }
 }
