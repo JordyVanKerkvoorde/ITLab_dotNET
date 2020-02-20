26f375db3e98c5283c4f27302d1db640b1d254a6
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-02-20 13:09:57 +0100

    started configuration

---
+++ ITLab29/Data/Mapping/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ITLab29.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLab29.Data.Mapping
+{
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasMany(t => t.Media)
+        }
+    }
+}
