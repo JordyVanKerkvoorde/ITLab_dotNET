3289964ef6b67854f8a12d17dd12b8f024820fe5
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-02-25 12:02:13 +0100

    css layout

--- ITLab29/Startup.cs
+++ ITLab29/Startup.cs
@@ -59,7 +59,7 @@ namespace ITLab29 {
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Session}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
 
