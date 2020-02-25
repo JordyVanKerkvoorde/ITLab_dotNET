ca057b1cf31f96bfa754b1a1cf9c0db36c980c27
VandeWoestyneYorick <yorick.vandewoestyne@student.hogent.be>
2020-02-25 11:07:06 +0100

    Add User-details to User/Index

--- ITLab29/Data/DataInitializer.cs
+++ ITLab29/Data/DataInitializer.cs
@@ -31,7 +31,11 @@ namespace ITLab29.Data
                 { UserName = "janw@gmail.com", Email = "janw@gmail.com", EmailConfirmed = true };
                 User dummyUser3 = new User("12345", "Sander", "Machado", UserType.RESPONSIBLE, UserStatus.BLOCKED)
                 { UserName = "sandercm@gmail.com", Email = "sandercm@gmail.com", EmailConfirmed = true };
-
+                Media avatar = new Media(MediaType.IMAGE, "~/photo/stock.png");
+                _dbContext.Media.Add(avatar);
+                dummyUser.Avatar = avatar;
+                dummyUser2.Avatar = avatar;
+                dummyUser3.Avatar = avatar;
 
                 await _userManager.CreateAsync(dummyUser, "P@ssword1");
                 await _userManager.CreateAsync(dummyUser2, "P@ssword1");
