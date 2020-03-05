2f90b301d7b6e7ef8086422d1eda982ad7fe3f25
Jan Willem <janwill1996@gmail.com>
2020-03-05 12:09:05 +0100

    Session controller try catch

--- ITLab29/Controllers/SessionController.cs
+++ ITLab29/Controllers/SessionController.cs
@@ -7,6 +7,7 @@ using ITLab29.Filters;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ITLab29.Models.ViewModels;
+using ITLab29.Exceptions;
 
 namespace ITLab29.Controllers
 {
@@ -25,10 +26,26 @@ namespace ITLab29.Controllers
         {
             IEnumerable<Session> sessions;
             if (date == null) {
-                sessions = _sessionRepository.GetAll();
+                try
+                {
+                    sessions = _sessionRepository.GetAll();
+                }
+                catch (EmptyListException e)
+                {
+                    Console.Error.WriteLine(e.StackTrace);
+                    sessions = new List<Session>();
+                }
             } else {
-                sessions = _sessionRepository.GetByDate(date??DateTime.Now);
-                //aanpassen na database met data of dummy dates ^^^^ 
+                try
+                {
+                    sessions = _sessionRepository.GetByDate(date ?? DateTime.Now);
+                    //aanpassen na database met data of dummy dates ^^^^ 
+                }
+                catch (EmptyListException e)
+                {
+                    Console.Error.WriteLine(e.StackTrace);
+                    sessions = new List<Session>();
+                }
             }
             sessions = sessions.OrderBy(s => s.Start).ToList();
             return View(sessions);
@@ -37,18 +54,34 @@ namespace ITLab29.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            IEnumerable<Session> sessions;
-            sessions = _sessionRepository.GetAll();
-            return Ok(sessions.OrderBy(s => s.Start).ToList());
+            try
+            {
+                IEnumerable<Session> sessions;
+                sessions = _sessionRepository.GetAll();
+                return Ok(sessions.OrderBy(s => s.Start).ToList());
+            }
+            catch (EmptyListException e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
+                // Eventueel nog aanpassen en ViewData gebruiken?
+                return Ok(new List<Session>());
+            }
+           
         }
         [Route("session/{id}")]
         [ServiceFilter(typeof(LoggedOnUserFilter))]
         public IActionResult Details(int id, User user) {
-            Session session = _sessionRepository.GetById(id);
-            if (session == null) {
+            Session session;
+            try
+            {
+                session = _sessionRepository.GetById(id);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
                 return NotFound();
             }
-
+            
             ViewData["user"] = user;
             ViewData["session"] = session;
             return View(new FeedBackViewModel());
@@ -57,10 +90,19 @@ namespace ITLab29.Controllers
         [HttpPost]
         [ServiceFilter(typeof(LoggedOnUserFilter))]
         public IActionResult Add(int id, User user) {
-            Session session = _sessionRepository.GetById(id);
+            Session session;
+            try
+            {
+                session = _sessionRepository.GetById(id);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
+                return NotFound();
+            }
             session.AddUserSession(user);
+            // maar 1 repository saven want die savet de context.
             _userRepository.SaveChanges();
-            _sessionRepository.SaveChanges();
 
             ViewData["sessionId"] = id;
             return RedirectToAction("Index");
@@ -69,11 +111,18 @@ namespace ITLab29.Controllers
         [HttpPost]
         [ServiceFilter(typeof(LoggedOnUserFilter))]
         public IActionResult Delete(int id, User user) {
-            Session session = _sessionRepository.GetById(id);
+            Session session;
+            try
+            {
+                session = _sessionRepository.GetById(id);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
+                return NotFound();
+            }
             user.RemoveUserSession(session);
-            session.RemoveUserSession(user);
             _userRepository.SaveChanges();
-            _sessionRepository.SaveChanges();
 
             return RedirectToAction("Index");
         }
@@ -83,7 +132,16 @@ namespace ITLab29.Controllers
             Console.WriteLine(feedback.id);
             Console.WriteLine(feedback.Score);
             Console.WriteLine(feedback.Description);
-            Session session = _sessionRepository.GetById(feedback.id);
+            Session session;
+            try
+            {
+                session = _sessionRepository.GetById(feedback.id);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
+                return NotFound();
+            }
             session.AddFeedback(new Feedback(feedback.Score, feedback.Description));
             _sessionRepository.SaveChanges();
             return RedirectToAction("Index");
